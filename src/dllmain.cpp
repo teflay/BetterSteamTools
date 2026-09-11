2f1cc84473008f905a5b7a3ae628ed4eb974534f
teflay <80451217+teflay@users.noreply.github.com>
2026-09-11 05:51:36 -0300

    Reduce sleep duration in ManifestSyncThread

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -133,7 +133,7 @@ static void SyncManifestCache()
 static void ManifestSyncThread()
 {
     while (true) {
-        std::this_thread::sleep_for(std::chrono::minutes(1));
+        std::this_thread::sleep_for(std::chrono::seconds(30));
         SyncManifestCache();
     }
 }
